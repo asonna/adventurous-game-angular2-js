2f279c4d23d67317b78cd54050fc471bf3ebf753
Annie Sonna and Brad Copenhaver <brad@>
2017-01-24 13:23:29 -0800

    add radio button choices to game view

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerService} from '../player.service';
+import {Player} from '../player.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
+  providers: [PlayerService]
 })
 export class GameComponent implements OnInit {
+  currentPlayer: Player;
 
-  constructor() { }
+  constructor(private playerService: PlayerService, private router: Router) { }
 
   ngOnInit() {
+    this.currentPlayer = this.playerService.getPlayer();
   }
 
+  crossRiver() {
+    if (this.currentPlayer.choice === "1"){
+      console.log(this.currentPlayer.choice);
+
+    }else if (this.currentPlayer.choice==="2") {
+      console.log(this.currentPlayer.choice);
+    }else {
+      console.log(this.currentPlayer.choice);
+    }
+  }
 }
--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,4 +1,5 @@
 export class Player {
   public points: number = 5;
+  public choice: string = "";
   constructor(public name: string, public height: number, public adventure: number){}
 }
